91cbce16162560dd4f2729be9ae4590b4746042c
SS <ss@inbox.ru>
2024-11-25 22:18:54 +0300

    Убрал ляп

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import psycopg2
-#print ('ЖОПА')
 try:
     # пытаемся подключиться к базе данных
     conn = psycopg2.connect(dbname='baturin', user='bat', password='mypassword', host='postgres-container', port='5432')
